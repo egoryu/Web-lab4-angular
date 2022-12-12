d3f95cb12c4220f03bd1e7cd398d22b70518dee3
egoryu <egornikitin1401@gmail.com>
2022-12-12 14:26:07 +0300

    Start

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lab4'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('lab4');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { MainComponent } from './components/main/main.component';
+import { PointService } from './services/point/point.service';
+import { UserService } from './services/user/user.service';
 
+const appRoutes: Routes = [
+  {path: "", component:IndexComponent},
+  {path: "main", component:MainComponent},
+  {path: "**", redirectTo: '/'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [PointService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
